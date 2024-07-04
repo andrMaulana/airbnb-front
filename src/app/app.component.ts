ccc5c3b4ca47fbf337a45c8d0c0080be6ea92d07
AndrMaulana <andryymaulana2000@gmail.com>
2024-07-04 23:24:39 +0700

    set app to import app.components.ts 🚀

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'airbnb-clone-front';
+export class AppComponent implements OnInit{
+  faIconLibrary: FaIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+      this.initFontAwesome()
+  }
+
+  private initFontAwesome() : void {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
 }
